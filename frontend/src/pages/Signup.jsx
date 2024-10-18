d6e0af93412291b4754ab8b197912e9a5e205b47
SAIRANTRI <pathaksairantri@gmail.com>
2024-10-18 16:59:10 +0530

    added password strength endpoint

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import Input from "../components/Input";
 import { Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter.jsx";
 
 const SignUpPage = () => {
 	const [name, setName] = useState("");
@@ -49,6 +50,8 @@ const SignUpPage = () => {
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
+
+					<PasswordStrengthMeter password={password} />
 
 					<motion.button
 						className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
